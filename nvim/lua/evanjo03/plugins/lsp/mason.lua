09853461c01b083a0af530dc3c49d188f9c94cf0
John Evans <john.evans@tsi.com>
2023-10-01 20:45:01 -0500

    Update

--- nvim/lua/evanjo03/plugins/lsp/mason.lua
+++ nvim/lua/evanjo03/plugins/lsp/mason.lua
@@ -39,7 +39,7 @@ mason_null_ls.setup({
     "prettier", -- ts/js formatter
     "stylua", -- lua formatter
     "eslint_d", -- ts/js linter
-    "gopls"
+    "golangci_lint" --golang linter
   },
   -- auto-install configured formatters & linters (with null-ls)
   automatic_installation = true,
