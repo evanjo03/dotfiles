6f958a07ae9acc670c300389f157f3e783dd4149
John Evans <john.evans@tsi.com>
2024-04-14 14:07:35 -0500

    updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -20,8 +20,8 @@ end
 -- config.color_scheme = "nightfox"
 -- config.color_scheme = "Kanagawa (Gogh)"
 -- config.color_scheme = "Catppuccin Frappé (Gogh)"
-config.color_scheme = "Catppuccin Macchiato (Gogh)"
--- config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Catppuccin Macchiato (Gogh)"
+config.color_scheme = "Catppuccin Mocha (Gogh)"
 -- config.color_scheme = "Night Owl (Gogh)"
 
 -- Change the font
