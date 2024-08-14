fa98e6df0946159860a97726deedab74e5b696ae
John Evans <john.evans@tsi.com>
2024-08-14 08:54:46 -0500

    Updates

--- .config/nvim.bak/lua/plugins/neo-tree.lua
+++ .config/nvim.bak/lua/plugins/neo-tree.lua
@@ -14,5 +14,16 @@ return {
         expander_expanded = "▼",
       },
     },
+    event_handlers = {
+      {
+        event = "file_opened",
+        handler = function(file_path)
+          -- auto close
+          -- vimc.cmd("Neotree close")
+          -- OR
+          require("neo-tree.command").execute({ action = "close" })
+        end,
+      },
+    },
   },
 }
