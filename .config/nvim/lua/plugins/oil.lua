756c59a0218d0f0b2783f4fb370ed097c04e013f
John Evans <john.evans@tsi.com>
2025-04-08 10:28:11 -0500

    Oil - show hidden files

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -9,7 +9,12 @@ return {
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+      },
+    }
 
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
   end,
