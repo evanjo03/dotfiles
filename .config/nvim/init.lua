745f13bfe03ce78887e4f7ad08fc380e9e7aa3cf
John Evans <john.evans@tsi.com>
2025-05-21 14:31:59 -0500

    Fix autocomplete

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,29 +1,29 @@
-require 'config.options'
-require 'config.globals'
-require 'config.autocommands'
-require 'config.lazy'
+require("config.options")
+require("config.globals")
+require("config.autocommands")
+require("config.lazy")
 
-require 'lazy'.setup({
-  { import = 'plugins.lsp' },
-  { import = 'plugins' },
+require("lazy").setup({
+	{ import = "plugins.lsp" },
+	{ import = "plugins" },
 }, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
+	ui = {
+		-- If you are using a Nerd Font: set icons to an empty table which will use the
+		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
 })
