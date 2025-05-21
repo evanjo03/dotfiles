6f5e57011972c9b1f496ddbfe2730286524094c3
John Evans <john.evans@tsi.com>
2025-05-21 14:25:07 -0500

    Blink autocomplete

--- .config/nvim/lua/plugins/lsp/lsp.lua
+++ .config/nvim/lua/plugins/lsp/lsp.lua
@@ -144,8 +144,7 @@ return {
 		--  By default, Neovim doesn't support everything that is in the LSP specification.
 		--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 		--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 		-- Enable the following language servers
 		--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -167,9 +166,9 @@ return {
 			--    https://github.com/pmizio/typescript-tools.nvim
 			--
 			-- But for many setups, the LSP (`ts_ls`) will work just fine
-			ts_ls = {
-				filetypes = { "typescript", "vue" },
-			},
+			-- ts_ls = {
+			-- 	filetypes = { "typescript", "vue" },
+			-- },
 
 			lua_ls = {
 				-- cmd = { ... },
