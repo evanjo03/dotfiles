deffbe4ceb4cc8562fa263b42a3bbfa4bd4af4ca
John Evans <john.evans@tsi.com>
2025-05-22 14:47:50 -0500

    Updates

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -19,3 +19,6 @@ opt.tabstop = 4 -- A TAB character looks like 4 spaces
 opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- smooth scroll
+opt.smoothscroll = true
