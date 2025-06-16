c108c58294430b9b90973e1473f1269865e4ea35
John Evans <john.wes.evans@gmail.com>
2025-06-16 18:57:03 -0500

    Updates

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "Mofiqul/vscode.nvim",
+    name = "vscode",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+}
