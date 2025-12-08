798b630760c28fc54d607fbcedfe9cf06b77615d
John Evans <john.evans@tsi.com>
2025-12-08 10:52:31 -0600

    Updates

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,13 @@
 return {
-  -- {
-  --   "Mofiqul/vscode.nvim",
-  --   name = "vscode",
-  --   priority = 1000,
-  -- },
-  -- {
-  --   "Mofiqul/vscode.nvim",
-  --   name = "vscode",
-  --   priority = 1000,
-  -- },
-  --
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = { flavour = "frappe" } },
+  -- { "Mofiqul/vscode.nvim", name = "vscode", priority = 1000 },
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = { flavour = "frappe" } },
   -- { "oxfist/night-owl.nvim", name = "night-owl", priority = 1000 },
   -- { "rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000 },
+  { "sainnhe/everforest", name = "everforest", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "everforest",
     },
   },
 }
