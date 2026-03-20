0697bb7aaaa21e6373d3eae2f81a4d623cf0d245
John Evans <john.evans@tsi.com>
2026-03-20 14:44:58 -0500

    Updates

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,12 +2,13 @@ return {
   -- { "Mofiqul/vscode.nvim", name = "vscode", priority = 1000 },
   -- { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = { flavour = "frappe" } },
   -- { "oxfist/night-owl.nvim", name = "night-owl", priority = 1000 },
-  -- { "rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000 },
-  { "sainnhe/everforest", name = "everforest", priority = 1000 },
+  -- { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
+  { "rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000 },
+  -- { "sainnhe/everforest", name = "everforest", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "catppuccin",
     },
   },
 }
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -20,9 +20,9 @@ end
 -- config.color_scheme = "Tokyo Night (Gogh)"
 -- config.color_scheme = "Vs Code Dark+ (Gogh)"
 -- config.color_scheme = "nightfox"
-config.color_scheme = "Everforest Dark (Gogh)"
+-- config.color_scheme = "Everforest Dark (Gogh)"
 -- config.color_scheme = "nord"
--- config.color_scheme = "Kanagawa (Gogh)"
+config.color_scheme = "Kanagawa (Gogh)"
 -- config.color_scheme = "Catppuccin Frappé (Gogh)"
 -- config.color_scheme = "Catppuccin Macchiato (Gogh)"
 -- config.color_scheme = "Catppuccin Mocha (Gogh)"
@@ -32,8 +32,8 @@ config.color_scheme = "Everforest Dark (Gogh)"
 -- config.font = wezterm.font("Hack Nerd Font")
 
 -- config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font = wezterm.font("Monaspace Neon NF")
--- config.font = wezterm.font("FiraCode Nerd Font")
+-- config.font = wezterm.font("Monaspace Neon NF")
+config.font = wezterm.font("FiraCode Nerd Font")
 -- config.font = wezterm.font("FiraMono Nerd Font")
 --
 config.font_size = 16
